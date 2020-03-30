87cc00bc914da913bef135037a398af4b1939c59
Fernando Costa <fernandocosta@Fernandos-MBP.home>
2020-03-30 18:44:04 +0200

    practice

--- module-1/lab-numpy/your-code/main.py
+++ module-1/lab-numpy/your-code/main.py
@@ -1,68 +1,93 @@
 #1. Import the NUMPY package under the name np.
 
-
+import numpy as np
 
 #2. Print the NUMPY version and the configuration.
 
-
+print(np.version.version)
 
 #3. Generate a 2x3x5 3-dimensional array with random values. Assign the array to variable "a"
 # Challenge: there are at least three easy ways that use numpy to generate random arrays. How many ways can you find?
 
+a = np.random.rand(2, 3, 5)
+
+"""a = np.random.rand(2,3,5)
+a = np.random.randint(50, size=(2,3,5))
+a = np.random.randn(2,3,5)
+
+1 ) números random aleatorios
+2) números random aleatorios pudiéndoles dar un mínimo y un máximo y el tamaño que tú quieras
+3) números random aleatorios que siguen distribución normal"""
 
 
 #4. Print a.
 
+print(a)
 
 
 #5. Create a 5x2x3 3-dimensional array with all values equaling 1.
 #Assign the array to variable "b"
 
-
+b = np.full((5, 2, 3), 1)
 
 #6. Print b.
 
-
+print(b)
 
 #7. Do a and b have the same size? How do you prove that in Python code?
 
-
-
+print(a.size == b.size)
 
 #8. Are you able to add a and b? Why or why not?
 
-
+#print(a + b)
 
 #9. Transpose b so that it has the same structure of a (i.e. become a 2x3x5 array). Assign the transposed array to varialbe "c".
-
-
+print("LA BÉ: ", b.shape)
+c = b.transpose(1, 2, 0)
+print(c)
 
 #10. Try to add a and c. Now it should work. Assign the sum to varialbe "d". But why does it work now?
 
-
+d = a + c
+print(d)
 
 #11. Print a and d. Notice the difference and relation of the two array in terms of the values? Explain.
-
-
-
+print("11")
+print("LA A", a)
+print("LA D", d)
 
 #12. Multiply a and c. Assign the result to e.
+print("12")
+print(a.shape)
+print(c.shape)
 
 
+e = a * c
+print("LA E", e)
 
 #13. Does e equal to a? Why or why not?
-
-
-
+print("EQUAL???")
+print(e.all==a.all)
+print(e==a)
+#Yes, because it is multiplied by 1
 
 #14. Identify the max, min, and mean values in d. Assign those values to variables "d_max", "d_min", and "d_mean"
 
+d_max = np.amax(d)
+d_min = np.amin(d)
+d_mean = np.mean(d)
+
+print(d_max, d_min, d_mean)
 
 
-
+"""d_min =
+d_mean = 
+"""
 #15. Now we want to label the values in d. First create an empty array "f" with the same shape (i.e. 2x3x5) as d using `np.empty`.
 
-
+f = np.empty([2, 3, 5])
+print(f)
 
 
 """
